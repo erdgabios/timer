1e4338fa826e35724886e1503547ef75c55df7b1
Gabriel Erdelyi <erdgabios@gmail.com>
2017-01-14 00:11:02 +0100

    Final

--- exercise/ViewController.swift
+++ exercise/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    var timer = Timer()
+    var counter = 0
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +28,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func updateTimer() {
+        
+        counter += 1
+        label.text = String(counter)
+        
+    }
+    
+    @IBAction func start(_ sender: Any) {
+        
+        counter = 0
+        label.text = String(counter)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        
+        timer.invalidate()
+    }
+    
+    
+    
+    
+    
+    
+    
 
 }
 
